73103184d603df0ee93db6946e71a3aee0e5119b
KizitoNaanma <naanma4kizito@gmail.com>
2024-01-08 19:29:36 +0100

    arketplace test

---
+++ scripts/marketplaceDeploy.ts
@@ -0,0 +1,22 @@
+// scripts/deploy.ts
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  
+  const marketplace = await Marketplace.deploy();
+  await marketplace.deployed();
+
+  console.log("Marketplace contract deployed to:", marketplace.address);
+}
+
+// Run the deployment script using ts-node
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
